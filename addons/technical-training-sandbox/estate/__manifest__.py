432c316e1c66e75c382070219ff3f12e593c6a2f
chfix <ischanfer@protonmail.com>
2023-09-16 11:21:28 +0100

    application folder created (exercice of chapter 3)

---
+++ addons/technical-training-sandbox/estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Real Estate by dosys",
+    'version': '1.0',
+    'author': "Consultant Odoo",
+    'category': 'Real Estate',
+    'depends': ['base'],
+    'website': 'www.dosys.ma',
+    'description': """
+    Description text of the real estate App
+    """,
+    'application': True
+
+
+}
