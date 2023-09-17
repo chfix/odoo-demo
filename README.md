4e8977adc8c79810377097d916b39010cc90b613
chfix <ischanfer@protonmail.com>
2023-09-17 14:05:02 +0100

    Views updated (exercice of chapter 6 & 7)

--- README.md
+++ README.md
@@ -5,9 +5,22 @@ Odoo demo is a test app to practise Odoo ERP developement.
 
 This Project is realised for <a href="https://www.doosys.ma">Doosys</a>,
 
-Chapters :
+# Chapters :
 ----------
 
-# Chapter 3 [Application initialisation] : Creation of the app manifest, and init file.
-# Chapter 4 [Models and basic fields] : Creation of EstateProperty Class, and adding the fields of the class and their parameters.
-# Chapter 5 [Access rights Intro] : Creation of the file "ir.model.access.csv" with read, write, create and unlink permissions to the group base.group_user.
+## Chapter 3 [Application initialisation] : 
+Creation of the app manifest, and init file.
+
+## Chapter 4 [Models and basic fields] : 
+Creation of EstateProperty Class, and adding the fields of the class and their parameters.
+
+## Chapter 5 [Access rights Intro] : 
+Creation of the file "ir.model.access.csv" with read, write, create and unlink permissions to the group base.group_user.
+
+## Chapter 6 [Setup the views folder] : 
+Creation of the view files ( menu xml file and declaration of properties view )
+
+## Chapter 7 [Basic views setup] : 
+Creation of the custom tree, the form and the search, the filter and the group functionalities included.
+
+## Chapter 8 [Models relations] : 
--- addons/technical-training-sandbox/estate/models/estate_property.py
+++ addons/technical-training-sandbox/estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -8,15 +9,28 @@ class EstateProperty(models.Model):
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Availability Date')
+    date_availability = fields.Date(
+        string='Available From',
+        default=lambda self: (fields.Date.today() + timedelta(days=90)).strftime('%Y-%m-%d'),
+        copy=False
+    )
     expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
-    living_area = fields.Integer(string='Living Area')
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
     garden = fields.Boolean(string='Garden')
-    garden_area = fields.Integer(string='Garden Area')
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection(
+        string='Status',
+        selection=[
+            ('new', 'New'), ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'),
+            ('canceled', 'Canceled')],
+        default='new',
+        copy=False)
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
